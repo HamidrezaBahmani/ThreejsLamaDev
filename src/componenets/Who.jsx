984953471ddc38f28a386e5789e7227e16e2fedb
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-08-25 00:09:57 +0430

    middle of project adding sections

--- src/componenets/Who.jsx
+++ src/componenets/Who.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+import { styled } from "styled-components";
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
 export const Who = () => {
-  return <div>Who</div>;
+  return <Section>Who</Section>;
 };
